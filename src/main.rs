4fd5e509fb390574352e89b008c85b04364cd842
ArunJot <arunjot@antiersolutions.com>
2024-04-14 15:01:45 +0530

    working on user input for the swap programs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 pub mod swap_two_numbers;
-use crate::swap_two_numbers::swap_numbers;
+use std::{io, process::exit};
+use crate::swap_two_numbers::swap_numbers_using_input;
 fn main() {
-    swap_numbers();
+    swap_numbers_using_input();
+    loop {
+        swap_numbers_using_input();
+        println!("Do you want to show the numbers again? (yes/no)");
+
+        let mut response = String::new();
+        io::stdin().read_line(&mut response).unwrap();
+        let response: String = response.trim().to_lowercase();
+
+        if response != "yes" {
+            break;
+        }
+    }
 }
--- src/swap_two_numbers.rs
+++ src/swap_two_numbers.rs
@@ -1,10 +1,74 @@
-pub fn swap_numbers(){
+use std::{io, process::exit};
+use lazy_static::lazy_static;
+
+#[allow(dead_code)]
+/// A normal swap between the two variables
+pub fn swap_numbers() {
     let mut number_one = 1;
     let mut number_second = 3;
-    println!("number_one {},number_two {}",number_one,number_second);
+    println!("number_one {},number_two {}", number_one, number_second);
     let swap_second = number_one;
     number_one = number_second;
     number_second = swap_second;
-    println!("number_one {},number_two {}",number_one,number_second);
+    println!("number_one {},number_two {}", number_one, number_second);
+}
 
+/// Swap using the user input
+pub fn swap_numbers_using_input() {
+    println!("Please choose if you want to input or want to Show the existing numbers ");
+
+    lazy_static! {
+        static ref FIRST_NUMBER: u32 = 0;
+        static ref SECOND_NUMBER: u32 = 0;
+    }
+    
+
+    loop {
+        let mut command = String::new();
+        io::stdin().read_line(&mut command).unwrap();
+        let command: String = command.to_lowercase().trim().parse().unwrap();
+
+        match command.as_ref() {
+            "show" => {
+                show();
+                break;
+            }
+            "input" => {
+                input();
+                break; // Exit the loop after input
+            }
+            _ => println!("invalid input"),
+        }
+    }
+
+
+    fn show() {
+        println!("First number: {}", *FIRST_NUMBER);
+        println!("Second number: {}", *SECOND_NUMBER);
+    }
+
+    fn input() {
+        println!("Please inter the first number");
+
+        let mut input_one = String::new();
+        io::stdin().read_line(&mut input_one).expect("Failed to read line");
+        let mut ref_first :u32 = *FIRST_NUMBER;
+        ref_first = input_one.trim().parse().expect("Please type a number!");
+
+        println!("Please inter the second number");
+        let mut second_one = String::new();
+        io::stdin().read_line(&mut second_one).expect("Failed to read line");
+        let mut ref_second:u32 = *SECOND_NUMBER;
+        ref_second = second_one
+            .trim()
+            .parse()
+            .expect("Please type a number in second integer!");
+
+        let swap_second_number = ref_first;
+        ref_first = ref_second;
+        ref_second = swap_second_number;
+
+        println!("thank you for storing the numbers");
+        
+    }
 }
