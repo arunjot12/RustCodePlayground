1a5c17a3a52e94b673a6e575ab2d9baf1536abf9
ArunJot <arunjot@antiersolutions.com>
2024-07-10 15:35:37 +0530

    modified the full pyramid program

--- src/patterns/full_pyramid.rs
+++ src/patterns/full_pyramid.rs
@@ -1,11 +1,16 @@
 pub fn full_pyramid() {
+     // Start the iterations 
     for i in 1..=5 {
-        for _ in 1..=5 - i {
+        // Print the space in between the numbers
+        for _ in 1..=(5 -i) {
             print!(" ");
         }
-        for _ in 1..(2 * i) {
-            print!("{}", i);
+        // Print the numbers 
+        for _ in 0..( i) {
+            print!("{} ", i);
         }
+       
+        // Change the line
         println!();
     }
 }
