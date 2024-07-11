5e21e1bb9ad0b8467c0eeed4ed7807542d440e21
ArunJot <arunjot@antiersolutions.com>
2024-07-11 17:26:53 +0530

    modified the square pyramid program

--- src/patterns/square_pyramid.rs
+++ src/patterns/square_pyramid.rs
@@ -1,17 +1,14 @@
 pub fn square_pyramid() {
     for i in 1..=5 {
         for k in 1..=5 {
-            if i == 1 || i == 5 {
-                print!("*");
+            if i == 1 || i == 5 ||  k==5 {
+                print!("* ");
             }
-            else if i >=2 && k==1   {
-                print!("*");
+            else if i >=2 && k == 1   {
+                print!("* ");
             } 
-            else if k==5 {
-                print!("*");
-            }
             else{
-                print!(" ");
+                print!("  ");
             }
         }
         println!();
