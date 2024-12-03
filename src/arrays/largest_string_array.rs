cc9c856a390348a86d59b714b9b17e9093a426ec
ArunJot <arunjot@antiersolutions.com>
2024-12-03 09:23:56 +0530

    chore: added the program to check largest string in array without using any library

--- src/arrays/largest_string_array.rs
+++ src/arrays/largest_string_array.rs
@@ -1,4 +1,5 @@
-pub fn largest_string_array(){
+// Largest array in string using len funtion
+pub fn largest_array(){
     let arr = ["arunjot","singh","antier","blockchain"];
     let mut largest_array = arr[0];
     for i in arr{
@@ -8,4 +9,22 @@ pub fn largest_string_array(){
     } 
     println!("Largest array is {}",largest_array);
 
+}
+
+// Largest array in string without using len function
+pub fn largest_string_array(){
+    let arr = ["arunjot","singh","antier","blockchain"];
+    let mut largest_array = arr[0];
+    let mut largest_length = 0;
+    for string in arr{
+        let mut current_length = 0; 
+        for _ in string.chars(){
+            current_length+=1;
+        }
+        if current_length > largest_length{
+            largest_array = string;
+            largest_length = current_length;
+        }
+    }
+    println!("The largest string is '{}', with length {}", largest_array, largest_length);
 }
