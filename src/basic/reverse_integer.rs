89c3ecc3472e867d1fbd4a18f6afc3e7131354e9
ArunJot <arunjot@antiersolutions.com>
2024-12-06 13:27:03 +0530

    fix: added the case of negative number in reverse integer

--- src/basic/reverse_integer.rs
+++ src/basic/reverse_integer.rs
@@ -1,12 +1,21 @@
 // Program to reverse the integer
-pub fn reverse_integer(){
-    let mut number = 65;
+pub fn reverse_integer() {
+    let mut number = -165;
+    let original_number= number.to_string().chars().count();
+    let char = original_number - 1;
     let mut reversed = 0;
-    while number > 0{
-        let digit = number % 10;
-        reversed = reversed * 10 + digit;
-        number /= 10;
+    if number < 0 {
+        for _ in 0..char  {
+            let digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number /= 10;
+        }
+    } else {
+        while number > 0 {
+            let digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number /= 10;
+        }
     }
-    println!("The reverse integer is {}",reversed)
-
+    println!("The reverse integer is {}", reversed)
 }
