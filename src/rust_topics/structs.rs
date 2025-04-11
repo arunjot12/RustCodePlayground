a1e27d279106b5726df5b907bd633f3e303269ea
ArunJot <arunjot@antiersolutions.com>
2025-04-11 17:27:35 +0530

    chore: Added another example of struct

--- src/rust_topics/structs.rs
+++ src/rust_topics/structs.rs
@@ -11,3 +11,32 @@ fn show_data() {
         atr: 592,
     };
 }
+
+// Another example 
+
+const MAXIMUM_POWER :u16 = 1000;
+enum VehicleKind{
+    Car,
+    Bike,
+    Truck
+}
+
+struct VehicleData {
+    kind: VehicleKind,
+    registration_year: u16,
+    registration_month : u8,
+    power:u16
+}
+
+fn vehicle(){
+let vehicle = VehicleData {
+    kind: VehicleKind::Car,
+    registration_year: 2003,
+    registration_month: 11,
+    power: 120,
+    };
+    
+    if vehicle.power > MAXIMUM_POWER {
+    println!("Too powerful");
+    }
+}
