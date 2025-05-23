0457067030d94e13a0380591b55b85b8f08d87d5
ArunJot <arunjot@antiersolutions.com>
2025-05-23 15:04:19 +0530

    chore: Added a simple example for sync

---
+++ src/sync__async/sync.rs
@@ -0,0 +1,15 @@
+// A example 
+
+fn start_working(){
+    println!(
+        "Working ->>>>>"
+    );
+    take_break();
+    println!("Thank you")
+}
+
+fn take_break(){
+    println!(
+        "Taking a Break"
+    )
+}
