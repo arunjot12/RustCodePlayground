2e4f0e2a39abd7acc223fb9a4cf8b85dd8844201
ArunJot <arunjot@antiersolutions.com>
2025-07-16 15:03:04 +0530

    chore: Added the addition of linked list

--- src/linked_list/new_list.rs
+++ src/linked_list/new_list.rs
@@ -1,12 +1,14 @@
 pub fn new_list() {
     println!("Hello welcome to the new list");
 
-    let new_list = ListNode::new(1).append(2).append(3);
-    let remove_list = new_list.remove(2).expect("Unable to remove");
-    remove_list.print();
+    let new_list = ListNode::new(2).append(4).append(3);
+    let second_list = ListNode::new(5).append(6).append(4);
 
+    let third = ListNode::add_two_numbers(Some(Box::new(new_list)), Some(Box::new(second_list)));
+    third.unwrap().print();
 }
 
+#[derive(Debug,Clone)]
 pub struct ListNode {
     pub val: i32,
     pub next: Option<Box<ListNode>>,
@@ -17,17 +19,34 @@ impl ListNode {
         ListNode { val, next: None }
     }
 
-   fn remove(self, val: i32) -> Option<Box<ListNode>> {
-    if self.val == val {
-        // Skip this node and return the rest
-        self.next
-    } else {
-        Some(Box::new(ListNode {
-            val: self.val,
-            next: self.next.map(|node| node.remove(val))?,
-        }))
+    pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        match (l1,l2){
+              (Some(n1), Some(n2)) => {
+                let sum = n1.val + n2.val;
+                 println!("Val 1: {}, Val 2: {}, Sum: {}", n1.val, n2.val, sum);
+                 Some(Box::new(ListNode {
+                val: sum,
+                next: Self::add_two_numbers(n1.next, n2.next),
+            }))   
+            }
+              _ => None, 
+        }
     }
-}
+
+    fn remove(self, val: i32) -> Option<Box<ListNode>> {
+        println!("self.val {}, val:  {}", self.val, val);
+        if self.val == val {
+            println!("Skipping");
+            // Skip this node and return the rest
+            self.next
+        } else {
+            println!("Continuing with others");
+            Some(Box::new(ListNode {
+                val: self.val,
+                next: self.next.map(|node| node.remove(val))?,
+            }))
+        }
+    }
 
     fn append(self, val: i32) -> ListNode {
         ListNode {
